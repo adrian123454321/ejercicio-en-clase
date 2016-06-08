7664af4bbb02ef942217d589e0b3481cad25a97b
adrian123454321 <adrian.lora12345@gmail.com>
2016-06-07 20:36:30 -0600

    registro de estudiantes

---
+++ registro/registro.py
@@ -0,0 +1,31 @@
+
+print("1) Crear estudiante")
+print("2) Ingresar notas")
+print("3) Reporte de notas")
+print("4) Salir")
+resp = int(input("Elija una opcion"))
+if resp == 1:
+	nombre=input("ingrese un nombre")
+		registro = {
+		"estudiante": "nombre",
+		}
+		print(registro["estudiante"])
+
+print("1) Crear estudiante")
+print("2) Ingresar notas")
+print("3) Reporte de notas")
+print("4) Salir")
+resp = int(input("Elija una opcion"))
+if resp == 2:
+	nota = int(input("igrese una nota")) 
+
+print("1) Crear estudiante")
+print("2) Ingresar notas")
+print("3) Reporte de notas")
+print("4) Salir")
+resp = int(input("Elija una opcion"))
+
+
+
+
+
