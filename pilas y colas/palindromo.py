3727d005a6d73a142d94f9d876255a54b5f546ad
adrian123454321 <adrian.lora12345@gmail.com>
2016-07-04 14:36:05 -0600

    palindromo

--- pilas y colas/palindromo.py
+++ pilas y colas/palindromo.py
@@ -2,11 +2,9 @@ def es_palindromo(palabra):
 	mi_cola=deque([])
 	mi_pila =[]
 	for i in palabra:
-		print(i)
+		pri      nt(i)
 	mi_cola.append(i)
 	mi_pila.append(i)	
 
-	while (mi_cola.popleft()) == (mi_pila.pop()):
+	while (mi_cola.popleft(i)) == (mi_pila.pop(i)):
 		print(true)
-
-
