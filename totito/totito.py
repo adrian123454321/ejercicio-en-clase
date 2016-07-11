78044c36b3ee90681cc0a5b10cc814ede3199540
adrian123454321 <adrian.lora12345@gmail.com>
2016-07-10 20:37:52 -0600

    totito

--- totito/totito.py
+++ totito/totito.py
@@ -5,11 +5,9 @@ lista=[
 ]
 print("turno 1")
 print(lista[0])
-
 print(lista[1])
-
 print(lista[2])
-
+if x=0 print(jelou)
 play1=input("Elija una casilla: ")
 if play1=="a":
 	lista[0][0]="x"
@@ -50,38 +48,38 @@ print("turno 2")
 play2=input("elija una casilla: ")
 
 if play2=="a":
-	lista[0][0]="o"
+	lista[0][0]="0"
 	
 
 if play2=="b":
-	lista[0][1]="o"
+	lista[0][1]="0"
 	
 
 if play2=="c":
-	lista[0][2]="o"
+	lista[0][2]="0"
 	
 
 if play2=="d":
-	lista[1][0]="o"
+	lista[1][0]="0"
 	
 
 if play2=="e":
-	lista[1][1]="o"
+	lista[1][1]="0"
 
 
 if play2=="f":
-	lista[1][2]="o"
+	lista[1][2]="0"
 	
 if play2=="g":
-	lista[2][0]="o"
+	lista[2][0]="0"
 
 
 if play2=="h":
-	lista[2][1]="o"
+	lista[2][1]="0"
 	
 
 if play2=="i":
-	lista[2][2]="o"
+	lista[2][2]="0"
 print(lista[0])
 print(lista[1])
 print(lista[2])
@@ -126,34 +124,33 @@ print("turno 2")
 play2=input("elija una casilla: ")
 
 if play2=="a":
-	lista[0][0]="o"
+	lista[0][0]="0"
 	
 if play2=="b":
-	lista[0][1]="o"
+	lista[0][1]="0"
 
 
 if play2=="c":
-	lista[0][2]="o"
+	lista[0][2]="0"
 
 
 if play2=="d":
-	lista[1][0]="o"
-
+	lista[1][0]="0"
 
 if play2=="e":
-	lista[1][1]="o"
+	lista[1][1]="0"
 	
 if play2=="f":
-	lista[1][2]="o"
+	lista[1][2]="0"
 	
 if play2=="g":
-	lista[2][0]="o"
+	lista[2][0]="0"
 
 if play2=="h":
-	lista[2][1]="o"
+	lista[2][1]="0"
 
 if play2=="i":
-	lista[2][2]="o"
+	lista[2][2]="0"
 print(lista[0])
 print(lista[1])
 print(lista[2])
@@ -199,38 +196,164 @@ print("turno 2")
 play2=input("elija una casilla: ")
 
 if play2=="a":
-	lista[0][0]="o"
+	lista[0][0]="0"
 	
 
 if play2=="b":
-	lista[0][1]="o"
+	lista[0][1]="0"
 	
 
 if play2=="c":
-	lista[0][2]="o"
+	lista[0][2]="0"
 	
 
 if play2=="d":
-	lista[1][0]="o"
+	lista[1][0]="0"
 	
 
 if play2=="e":
-	lista[1][1]="o"
+	lista[1][1]="0"
 
 
 if play2=="f":
-	lista[1][2]="o"
+	lista[1][2]="0"
 	
 if play2=="g":
-	lista[2][0]="o"
+	lista[2][0]="0"
 
 
 if play2=="h":
-	lista[2][1]="o"
+	lista[2][1]="0"
 	
 
 if play2=="i":
-	lista[2][2]="o"
+	lista[2][2]="0"
+print(lista[0])
+print(lista[1])
+print(lista[2])
+
+print("turno 1")
+print(lista[0])
+
+print(lista[1])
+
+print(lista[2])
+
+play1=input("Elija una casilla: ")
+if play1=="a":
+	lista[0][0]="x"
+	
+
+if play1=="b":
+	lista[0][1]="x"
+	
+
+if play1=="c":
+	lista[0][2]="x"
+	
+
+if play1=="d":
+	lista[1][0]="x"
+	
+if play1=="e":
+	lista[1][1]="x"
+
+
+if play1=="f":
+	lista[1][2]="x"
+
+if play1=="g":
+	lista[2][0]="x"
+	
+if play1=="h":
+	lista[2][1]="x"
+
+
+if play1=="i":
+	lista[2][2]="x"
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print("turno 2")
+
+play2=input("elija una casilla: ")
+
+if play2=="a":
+	lista[0][0]="0"
+	
+
+if play2=="b":
+	lista[0][1]="0"
+	
+
+if play2=="c":
+	lista[0][2]="0"
+	
+
+if play2=="d":
+	lista[1][0]="0"
+	
+
+if play2=="e":
+	lista[1][1]="0"
+
+
+if play2=="f":
+	lista[1][2]="0"
+	
+if play2=="g":
+	lista[2][0]="0"
+
+
+if play2=="h":
+	lista[2][1]="0"
+	
+
+if play2=="i":
+	lista[2][2]="0"
+print(lista[0])
+print(lista[1])
+print(lista[2])
+
+print("turno 1")
+print(lista[0])
+
+print(lista[1])
+
+print(lista[2])
+
+play1=input("Elija una casilla: ")
+if play1=="a":
+	lista[0][0]="x"
+	
+
+if play1=="b":
+	lista[0][1]="x"
+	
+
+if play1=="c":
+	lista[0][2]="x"
+	
+
+if play1=="d":
+	lista[1][0]="x"
+	
+if play1=="e":
+	lista[1][1]="x"
+
+
+if play1=="f":
+	lista[1][2]="x"
+
+if play1=="g":
+	lista[2][0]="x"
+	
+if play1=="h":
+	lista[2][1]="x"
+
+
+if play1=="i":
+	lista[2][2]="x"
 print(lista[0])
 print(lista[1])
 print(lista[2])
