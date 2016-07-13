c640414441fc28b3aac658bc6c6c571ace0bf1aa
adrian123454321 <adrian.lora12345@gmail.com>
2016-07-12 20:39:33 -0600

    totito

--- totito/totito2.py
+++ totito/totito2.py
@@ -13,6 +13,18 @@ tirovalor=ord(turno1)-65
 tirofila=(tirovalor %  3)
 tirocolumna=(tirovalor // 3)
 
+if totito[0][0]="x" and totito[0][1]="x" and totito[0][2]="x"
+print("jugador1 gana ")
+
+if totito[1][0]= "x" and totito[1][1] and totito[1][2]="x"
+print("jugador1 gana")
+
+if totito[2][0]="x" and totito[2][1]="x" and totito[2][2]="x"
+print("jugador1 gana")
+
+
+
+print("turno2")
 print(totito[0])
 print(totito[1])
 print(totito[2])
@@ -21,3 +33,7 @@ turno2=input("elija una casilla ")
 tirovalor=ord(turno2)-65
 tirofila=(tirovalor % 3)
 tirocolumna=(tirovalor//3)
+
+print(totito[0])
+print(totito[1])
+print(totito[2])
