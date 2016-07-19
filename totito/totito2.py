b76158639092448e4a92d0283ea5a208e3c1fa60
adrian123454321 <adrian.lora12345@gmail.com>
2016-07-18 20:35:08 -0600

    totito

--- totito/totito2.py
+++ totito/totito2.py
@@ -3,26 +3,30 @@ totito=[
 ["d","e","f"],
 ["g","h","i"]
 ]
+while totito==0:
 
+	print("turno1")
 print(totito[0])
 print(totito[1])
 print(totito[2])
+
+Toto=[
+	[0,0,0].
+	[0,0,0],
+	[0,0,0]
+]
+
+print(w[0])
+print(w[1])
+print(w[2])
+Toto=True
+while Toto==True:
 
 turno1=input("elija una casilla ")
 tirovalor=ord(turno1)-65
 tirofila=(tirovalor %  3)
 tirocolumna=(tirovalor // 3)
-
-if totito[0][0]="x" and totito[0][1]="x" and totito[0][2]="x"
-print("jugador1 gana ")
-
-if totito[1][0]= "x" and totito[1][1] and totito[1][2]="x"
-print("jugador1 gana")
-
-if totito[2][0]="x" and totito[2][1]="x" and totito[2][2]="x"
-print("jugador1 gana")
-
-
+totito[tirocolumna][tirofila]=="x"
 
 print("turno2")
 print(totito[0])
@@ -33,7 +37,18 @@ turno2=input("elija una casilla ")
 tirovalor=ord(turno2)-65
 tirofila=(tirovalor % 3)
 tirocolumna=(tirovalor//3)
+totito[tirocolumna][tirofila]=="0"
 
 print(totito[0])
 print(totito[1])
 print(totito[2])
+
+if totito[0][0]=="x" and totito[0][1]=="x" and totito[0][2]=="x":
+print("Jugador1 gana")
+
+if totito[1][0]=="x" and totito[1][1]=="x" and totito[1][2]=="x":
+print("Jugador1 gana")
+if totito[2][0]=="x" and totito[2][1]=="x" and totito[2][2]=="x":
+print("Jugador1 gana")
+
+
